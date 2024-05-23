dcbedd4eb567679a48ee84e3f3d8d53cd53ece63
sam <samridhi@hdiw.jdo>
2024-05-24 00:31:33 +0530

    Section6

--- mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
+++ mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
@@ -4,13 +4,22 @@ package com.appsdeveloperblog.app.ws.ui.controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("users") //http://localhost:8080/users
+@RequestMapping("users") //http://localhost:8080/users // mapped with this
 public class UserController {
 
     @GetMapping
-    public String getUser()
+    public String getUsers(@RequestParam(value = "page" ,defaultValue = "0")  int page ,
+                           @RequestParam(value ="limit" , defaultValue = "0") int limit ,
+                           @RequestParam(value ="sort" ,  defaultValue = "Hello" ,required = false)  String sort )
     {
-        return "get user was called";
+        return "get user was called with page = " + page + "and limit =" + limit + "and sort =" + sort;
+    }
+
+
+    @GetMapping(path = "/{userId}" )
+    public String getUser(@PathVariable String userId)
+    {
+        return "get user was called with user Id " + userId;
     }
 
     @PostMapping
