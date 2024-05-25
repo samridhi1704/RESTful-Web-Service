cdbd838f9f3ef55d4dace3cd272725638ad6e154
sam <samridhi@hdiw.jdo>
2024-05-25 12:57:35 +0530

    Custom Error message

--- mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/exceptions/AppExceptionsHandler.java
+++ mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/exceptions/AppExceptionsHandler.java
@@ -1,5 +1,6 @@
 package com.appsdeveloperblog.app.ws.exceptions;
 
+import com.appsdeveloperblog.app.ws.ui.model.response.ErrorMessage;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.Date;
+
 @ControllerAdvice
 
 public class AppExceptionsHandler extends ResponseEntityExceptionHandler {
@@ -15,8 +18,16 @@ public class AppExceptionsHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(value = {Exception.class})
     public ResponseEntity<Object> handleAnyException(Exception ex , WebRequest request)
     {
+
+        String errorMessageDescription = ex.getLocalizedMessage();
+
+        if(errorMessageDescription == null)
+            errorMessageDescription = ex.toString();
+
+
+        ErrorMessage errorMessage = new ErrorMessage(new Date() , errorMessageDescription);
         return new ResponseEntity<>(
-                ex , new HttpHeaders() , HttpStatus.INTERNAL_SERVER_ERROR);
+                errorMessage , new HttpHeaders() , HttpStatus.INTERNAL_SERVER_ERROR);
 
     }
 
